ea068363c59a193eb03f97a5c340fc30c518cf67
Emmanuel Akintunde <akintundeemmanuel14@gmail.com>
2026-08-27 21:00:03 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,4 +37,6 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+    implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
 }
