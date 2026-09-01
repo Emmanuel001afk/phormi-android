849c479c7aff7d6f065c6e2d7c40795f0b2cf23f
Emmanuel Akintunde <akintundeemmanuel14@gmail.com>
2026-09-01 07:01:47 -0700

    Add APIVpnProfile class implementing Parcelable

---
+++ app/src/main/java/de/blinkt/openvpn/api/APIVpnProfile.java
@@ -0,0 +1,37 @@
+package de.blinkt.openvpn.api;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/** Parcelable companion required by the OpenVPN for Android remote AIDL API. */
+public class APIVpnProfile implements Parcelable {
+    public final String mUUID;
+    public final String mName;
+    public final boolean mUserEditable;
+
+    public APIVpnProfile(Parcel in) {
+        mUUID = in.readString();
+        mName = in.readString();
+        mUserEditable = in.readInt() != 0;
+    }
+
+    public APIVpnProfile(String uuidString, String name, boolean userEditable) {
+        mUUID = uuidString;
+        mName = name;
+        mUserEditable = userEditable;
+    }
+
+    @Override public int describeContents() { return 0; }
+
+    @Override public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(mUUID);
+        dest.writeString(mName);
+        dest.writeInt(mUserEditable ? 1 : 0);
+    }
+
+    public static final Parcelable.Creator<APIVpnProfile> CREATOR =
+        new Parcelable.Creator<APIVpnProfile>() {
+            @Override public APIVpnProfile createFromParcel(Parcel in) { return new APIVpnProfile(in); }
+            @Override public APIVpnProfile[] newArray(int size) { return new APIVpnProfile[size]; }
+        };
+}
