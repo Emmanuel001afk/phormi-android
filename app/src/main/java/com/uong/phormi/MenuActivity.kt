ca5f787387bf0afc91897cb36644ae4b8fa013e0
Emmanuel Akintunde <akintundeemmanuel14@gmail.com>
2026-09-09 14:18:30 +0100

    Wire Disposable Email Lab into browser menu

--- app/src/main/java/com/uong/phormi/MenuActivity.kt
+++ app/src/main/java/com/uong/phormi/MenuActivity.kt
@@ -54,6 +54,7 @@ class MenuActivity : AppCompatActivity() {
         wire(R.id.menu_history) { startActivityForResult(Intent(this, HistoryActivity::class.java), REQ_NESTED) }
         wire(R.id.menu_vpn) { startActivity(Intent(this, VpnActivity::class.java)) }
         wire(R.id.menu_ai) { startActivity(Intent(this, AiActivity::class.java)) }
+        wire(R.id.menu_disposable_email) { startActivity(Intent(this, PhormiDisposableEmailActivity::class.java)) }
         wire(R.id.menu_keyboard) { startActivity(Intent(this, PhormiKeyboardSettingsActivity::class.java)) }
         wire(R.id.menu_default_browser) { PhormiDefaultBrowserController.request(this) }
         wire(R.id.menu_browser_lock) { finishWith(ACTION_BROWSER_LOCK) }
