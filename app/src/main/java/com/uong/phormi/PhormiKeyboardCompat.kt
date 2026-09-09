fdb64d880b19beaf45014171f857dbaf6409d449
Emmanuel Akintunde <akintundeemmanuel14@gmail.com>
2026-09-10 00:24:22 +0100

    Point keyboard compatibility accessor at V2 IME

--- app/src/main/java/com/uong/phormi/PhormiKeyboardCompat.kt
+++ app/src/main/java/com/uong/phormi/PhormiKeyboardCompat.kt
@@ -3,7 +3,7 @@ package com.uong.phormi
 import android.view.View
 
 /** Compatibility accessor for InputMethodService's private input view across Android versions. */
-fun PhormiKeyboardService.getInputView(): View? {
+fun PhormiKeyboardServiceV2.getInputView(): View? {
     var type: Class<*>? = javaClass
     while (type != null) {
         val field = type.declaredFields.firstOrNull { it.name == "mInputView" || it.name == "inputView" }
